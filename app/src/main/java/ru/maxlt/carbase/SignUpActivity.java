33267d96ba843300b0dcc719bcc3e7de390808a3
MacMax <e3162608@student.dcccd.edu>
2018-08-14 22:30:38 -0500

    ...

--- app/src/main/java/ru/maxlt/carbase/SignUpActivity.java
+++ app/src/main/java/ru/maxlt/carbase/SignUpActivity.java
@@ -1,13 +1,70 @@
 package ru.maxlt.carbase;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    FirebaseAuth mAuth;
+
+    @BindView(R.id.full_name_edittext)
+    TextInputEditText mFullName;
+    @BindView(R.id.email_textedit)
+    TextInputEditText mEmail;
+    @BindView(R.id.password_textedit)
+    TextInputEditText mPassword;
+    @BindView(R.id.confirm_password_textedit)
+    TextInputEditText mConfirmedPassword;
+    @BindView(R.id.sign_up_btn)
+    ImageButton mSignUpButton;
+    @BindView(R.id.progressBar_sign_up)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        ButterKnife.bind(this);
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fullName = mFullName.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String confirmedPassword = mConfirmedPassword.getText().toString().trim();
+
+                mProgressBar.setVisibility(View.VISIBLE);
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(SignUpActivity.this,"Account with email " + email + " has been created",Toast.LENGTH_SHORT).show();
+                        mProgressBar.setVisibility(View.GONE);
+                        if (!task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this,"Failed to create an account",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
