6bf7038965c9fdfa714058a16b82388ec129cf64
maxxlt <vipo9x@gmail.com>
2018-08-16 12:46:50 -0500

    ...

--- app/src/main/java/ru/maxlt/carbase/MainActivity.java
+++ app/src/main/java/ru/maxlt/carbase/MainActivity.java
@@ -1,14 +1,8 @@
 package ru.maxlt.carbase;
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
@@ -31,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signInButtonClicked(View view) {
-        buttonEffect(view);
         Toast.makeText(getBaseContext(), "Sign in btn_sign_in_background clicked", Toast.LENGTH_LONG).show();
     }
 
@@ -47,27 +40,5 @@ public class MainActivity extends AppCompatActivity {
     public void onSignUpTextClicked(View view) {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
-    }
-
-    //https://stackoverflow.com/questions/7175873/click-effect-on-button-in-android
-    public static void buttonEffect(View button){
-        button.setOnTouchListener(new View.OnTouchListener() {
-
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: {
-                        v.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP);
-                        v.invalidate();
-                        break;
-                    }
-                    case MotionEvent.ACTION_UP: {
-                        v.getBackground().clearColorFilter();
-                        v.invalidate();
-                        break;
-                    }
-                }
-                return false;
-            }
-        });
     }
 }
