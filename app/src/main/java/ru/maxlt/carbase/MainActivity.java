79163a3e347050b9fcb23bc581789c66aa1cb8d9
maxxlt <vipo9x@gmail.com>
2018-08-16 02:25:41 -0500

    ...

--- app/src/main/java/ru/maxlt/carbase/MainActivity.java
+++ app/src/main/java/ru/maxlt/carbase/MainActivity.java
@@ -1,19 +1,27 @@
 package ru.maxlt.carbase;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.remember_me_checkbox) CheckBox mRememberMeCheckbox;
-
+    @BindView(R.id.remember_me_checkbox)
+    CheckBox mRememberMeCheckbox;
+    @BindView(R.id.logo_iv)
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +31,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signInButtonClicked(View view) {
-        Toast.makeText(getBaseContext(),"Sign in button clicked",Toast.LENGTH_LONG).show();
+        buttonEffect(view);
+        Toast.makeText(getBaseContext(), "Sign in btn_sign_in_background clicked", Toast.LENGTH_LONG).show();
     }
 
     public void onPasswordForgotten(View view) {
-        Toast.makeText(getBaseContext(),"Forgot password button clicked",Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "Forgot password btn_sign_in_background clicked", Toast.LENGTH_LONG).show();
     }
 
     public void onRememberMeChecked(View view) {
         if (mRememberMeCheckbox.isChecked())
-            Toast.makeText(getBaseContext(),"Checked",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getBaseContext(), "Checked", Toast.LENGTH_SHORT).show();
     }
 
     public void onSignUpTextClicked(View view) {
-        Intent intent = new Intent(this,SignUpActivity.class);
+        Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
+    }
+
+    //https://stackoverflow.com/questions/7175873/click-effect-on-button-in-android
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/ru/maxlt/carbase/SignUpActivity.java
+++ app/src/main/java/ru/maxlt/carbase/SignUpActivity.java
@@ -1,11 +1,14 @@
 package ru.maxlt.carbase;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -15,11 +18,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
+    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    private Matcher matcher;
 
     FirebaseAuth mAuth;
 
@@ -35,6 +43,12 @@ public class SignUpActivity extends AppCompatActivity {
     ImageButton mSignUpButton;
     @BindView(R.id.progressBar_sign_up)
     ProgressBar mProgressBar;
+    @BindView(R.id.email_textinputlayout)
+    TextInputLayout mEmailTextLayout;
+    @BindView(R.id.password_textinputlayout)
+    TextInputLayout mPasswordTextLayout;
+    @BindView(R.id.confirm_password_textinputlayout)
+    TextInputLayout mConfirmedPasswordTextLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,24 +61,22 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String fullName = mFullName.getText().toString().trim();
                 final String email = mEmail.getText().toString().trim();
-                String password = mPassword.getText().toString().trim();
-                String confirmedPassword = mConfirmedPassword.getText().toString().trim();
-
+                final String password = mPassword.getText().toString().trim();
+                final String confirmedPassword = mConfirmedPassword.getText().toString().trim();
+                hideKeyboard();
                 setmProgressBar(View.VISIBLE);
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        clearText();
-                        setmProgressBar(View.GONE);
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(SignUpActivity.this, "Failed to create an account", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Intent mIntent = new Intent(SignUpActivity.this,
-                                    SignUpSuccessfulActivity.class);
-                            startActivity(mIntent);
-                        }
-                    }
-                });
+                if (!validateEmail(email)) {
+                    mEmailTextLayout.setError("Invalid email. Please type again.");
+                    setmProgressBar(View.GONE);
+                } else if (!validatePassword(password)) {
+                    mPasswordTextLayout.setError("Invalid password. Password should be more than 5 characters.");
+                    setmProgressBar(View.GONE);
+                } else if (!validateConfirmedPassword(confirmedPassword, password)) {
+                    mConfirmedPasswordTextLayout.setError("Invalid confirmed password. It must match your typed password.");
+                    setmProgressBar(View.GONE);
+                } else {
+                   signUp(email,password);
+                }
 
             }
         });
@@ -79,5 +91,46 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void setmProgressBar(int visibility) {
         mProgressBar.setVisibility(visibility);
+    }
+
+    //https://code.tutsplus.com/tutorials/creating-a-login-screen-using-textinputlayout--cms-24168
+    private void hideKeyboard() {
+        View view = getCurrentFocus();
+        if (view != null) {
+            ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).
+                    hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    private boolean validateEmail(String email) {
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean validatePassword(String password) {
+        return password.length() > 5;
+    }
+
+    private boolean validateConfirmedPassword(String confirmedPassword, String password) {
+        return confirmedPassword.equals(password);
+    }
+
+    private void signUp(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                clearText();
+                setmProgressBar(View.GONE);
+
+
+                if (!task.isSuccessful()) {
+                    Toast.makeText(SignUpActivity.this, "Failed to create an account", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent mIntent = new Intent(SignUpActivity.this,
+                            SignUpSuccessfulActivity.class);
+                    startActivity(mIntent);
+                }
+            }
+        });
     }
 }
